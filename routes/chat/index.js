08fed312d03af409ac10227cffdd8541c62596c2
Linh Tran <linh.mtran168@live.com>
2012-11-13 18:30:54 +0700

    add socket behavior

--- routes/chat/index.js
+++ routes/chat/index.js
@@ -5,24 +5,32 @@ var redis = require('redis')
   , Chat = require('../../models/chat');
 
 module.exports = function(io) {
-  // Socket io handling
+  /**
+   * Socket io handling
+   */
   io.sockets.on('connection', function(socket) {
 
     // Log
-    console.log('New user with socketId: ' + socket.id + ':' + socket.handshake.userId);
-    // Save sockedId to the database
-    Chat.saveUserSocketId(socket.handshake.userId, socket.id);
+    console.log('New user with socketId: ' + socket.id + ' : ' + socket.handshake.userId);
+    // When there is a new connection, save newsockedId to the database
+    Chat.saveUserSockId(socket.handshake.userId, socket.id);
 
-    // When there is a new message
+    // Emit welcome event to the client
+    socket.emit('welcome', { message: 'You have connected to socket service' });
+    /**
+     * Function to handle when there is a new message
+     * @param <Object> data { senderId: '', timestamp: '', message: '' }
+     */
     socket.on('message', function(data) {
-
+      console.log(data);
     });
 
     // When socket disconnect
     socket.on('disconnect', function() {
       // Log
       console.log('Disconnect, to delete: ' + socket.handshake.userId);
-      Chat.removeUserSocketId(socket.handshade.userId);
+      // When disconnect remove the socketId from the database
+      Chat.removeUserSockId(socket.handshake.userId);
     });
   });
 };
