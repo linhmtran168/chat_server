3fc594ae756d6da609f648822a9b976f999f3c95
Linh Tran <linh.mtran168@live.com>
2012-11-14 12:19:23 +0700

    Add save, send message function

--- models/chat.js
+++ models/chat.js
@@ -12,12 +12,34 @@ module.exports = {
 
   /**
    * Function to save sockId to redis
-   * @param <String> userId, <String> sockId
+   * @param <String> userId
+   * @param <String> sockId
    * @return void
    */
   saveUserSockId: function(userId, sockId) {
     redisClient.set('chat:' + userId + ':sockId', sockId, redis.print);
   },
+
+  /**
+   * Check if receiverId is online or not (in socketlist)
+   * @param <String> receiverId
+   */
+  checkReceiverStatus: function(receiverId, callback) {
+    redisClient.get('chat:' + receiverId + ':sockId', function(err, response) {
+      // If err
+      if (err) {
+        return callback(err, false);
+      }
+
+      // If this key not set
+      if (!response) {
+        return callback(null, null);
+      }
+
+      return callback(null, response);
+    });
+  },
+
 
   /**
    * Function to remove user's sockID from database
@@ -26,5 +48,85 @@ module.exports = {
    */
   removeUserSockId: function(userId) {
     redisClient.del('chat:' + userId + ':sockId', redis.print);
+  },
+
+  /**
+   * Function to add a message to message list
+   * @param <String> senderId
+   * @param <String> receiverId
+   * @param <String> message
+   * @param <String> timestamp
+   * @param <Function> callback
+   * @return void
+   */
+  addMessageToConversation: function(senderId, receiverId, message, timestamp, callback) {
+    // Create the message object
+    var messageObj = {
+      message: message,
+      timestamp: timestamp,
+      senderId: senderId,
+    };
+
+    // Save the new message obj to list of the messages of 2 user
+    // Sender
+    redisClient.lpush('chat:' + senderId + ':' + receiverId + ':' + 'messages', JSON.stringify(messageObj), function(err, response) {
+      // If error
+      if (err) {
+        return callback(err, false);
+      }
+
+      // Return the response
+      return callback(null, response);
+    });
+
+    // Receiver
+    redisClient.lpush('chat:' + receiverId + ':' + senderId + ':' + 'messages', JSON.stringify(messageObj), function(err, response) {
+      // If error
+      if (err) {
+        return callback(err, false);
+      }
+
+      // Return the response
+      return callback(null, response);
+    });
+  },
+
+  /**
+   * Function to create a new conversation
+   * @param <String> userId
+   * @param <String> senderId
+   * @param <String> timestamp
+   * @param <String> message
+   * @param <Function> callback
+   * @return void
+   */
+  updateConversationList: function(senderId, receiverId, message, timestamp, callback) {
+    // Create arguments array for 2 list sender and receiver
+    var argsSender = ['chat:' + senderId + ':conversations', parseInt(timestamp, 10), receiverId];
+    var argsReceiver = ['chat:' + receiverId + ':conversations', parseInt(timestamp, 10), senderId];
+
+    // Add/update the new conversation to the list of sender conversations
+    redisClient.zadd(argsSender, function(err, response) {
+      if (err) {
+        return callback(err, false);
+      }
+
+      // Set the last message
+      redisClient.set('chat:' + senderId + ':' + receiverId + ':' + 'lastMessage', message, redis.print);
+
+      return callback(null, response);
+    });
+
+    // Add/update the new conversation to the list of receiver conversations
+    redisClient.zadd(argsReceiver, function(err, response) {
+      if (err) {
+        return callback(err, false);
+      }
+
+      // Set the last message
+      redisClient.set('chat:' + receiverId + ':' + senderId + ':' + 'lastMessage', message, redis.print);
+
+      return callback(null, response);
+    });
   }
 };
--- routes/chat/index.js
+++ routes/chat/index.js
@@ -9,7 +9,6 @@ module.exports = function(io) {
    * Socket io handling
    */
   io.sockets.on('connection', function(socket) {
-
     // Log
     console.log('New user with socketId: ' + socket.id + ' : ' + socket.handshake.userId);
     // When there is a new connection, save newsockedId to the database
@@ -17,12 +16,67 @@ module.exports = function(io) {
 
     // Emit welcome event to the client
     socket.emit('welcome', { message: 'You have connected to socket service' });
+
     /**
      * Function to handle when there is a new message
-     * @param <Object> data { senderId: '', timestamp: '', message: '' }
+     * @param <Object> data { receiverId: '', timestamp: '', message: '' }
      */
     socket.on('message', function(data) {
-      console.log(data);
+      console.log('Data send from client' + data);
+      //---- Send message to the receiver if he/she is online
+      Chat.checkReceiverStatus(data.receiverId, function(err, receiverSockId) {
+        // If there is a error
+        if (err) {
+          socket.emit('error', { message: 'There is error getting socketId from database' });
+          throw err;
+        }
+
+        // Else
+        if (!receiverSockId) {
+          console.log('User is offline');
+          // Emit notify offline event to the client that send the message
+
+          socket.emit('notify-offline', { message: 'The user you want to send message is offline, he/she will receive your message when he/she is online again'  });
+        } else {
+          console.log('Successfully getting socket id: ' + receiverSockId + ' of user id: ' + data.receiverId);
+
+          // Create the message object
+          var messageObj = {
+            senderId: socket.handshake.userId,
+            timestamp: data.timestamp,
+            message: data.message
+          };
+
+          // Send the message to the receiver
+          io.sockets.socket(receiverSockId).emit('message-arrived', messageObj);
+        }
+        
+      });
+
+      //---- Save new conversation to the database
+      Chat.updateConvesationList(socket.handshake.userId, data.receiverId, data.message, data.timestamp, function(err, response) {
+        // If there is a error
+        if (err) {
+          socket.emit('error', { message: 'There is error saving message to database' });
+          throw err;
+        }
+
+        // Else
+        console.log('Reponse success: ' + response);
+      });
+
+      //---- Save new message to database
+      Chat.addMessageToConversation(socket.handshake.userId, data.receiverId, data.message, data.timestamp, function(err, response) {
+        // If there is a error
+        if (err) {
+          socket.emit('error', { message: 'There is error saving message to database' });
+          throw err;
+        }
+
+        // Else
+        console.log('Reponse success: ' + response);
+      });
+
     });
 
     // When socket disconnect
