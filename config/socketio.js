bb98cb91b6e66172790055b9e1b1a32c485f9468
Linh Tran <linh.mtran168@live.com>
2012-12-06 16:15:52 +0700

    add reccent chats page

--- config/socketio.js
+++ config/socketio.js
@@ -36,9 +36,8 @@ module.exports = function(io, redisStore, cookieParser) {
 
     // Authorization for socketio
     io.set('authorization', function(handshakeData, callback) { 
-      if (_.isUndefined(handshakeData.query.userId)) {
-        console.log(handshakeData.query.accessToken);
-
+      // If it is a request from mobile
+      if (handshakeData.query.accessToken) {
         // Find the user with the accessToken in the mongo database
         User.findOne({ 'accessToken': handshakeData.query.accessToken }, function(err, user) {
           // If an error occures
@@ -46,6 +45,7 @@ module.exports = function(io, redisStore, cookieParser) {
             return callback(err);
           }
 
+          console.log(user);
           // If there is no user with this accessToken
           if (!user) {
             return callback('There is no user with this accessToken', false);
@@ -54,12 +54,41 @@ module.exports = function(io, redisStore, cookieParser) {
           // Add the userId to the handshake data
           handshakeData.userId = user.id;
           // Process to connect to the socket
-          callback(null, true);
+          return callback(null, true);
         });
       } else {
-        // For test
-        handshakeData.userId = handshakeData.query.userId;
-        callback(null, true);
+        // If it is a request from the portal
+        // Use cookie to get the session id
+        // If there is no user id with the request, decline the request
+        console.log(handshakeData.query.userId + ' is trying to connect');
+        if (!handshakeData.query.userId) {
+          return callback(err, false);
+        }
+
+        cookieParser(handshakeData, {}, function(err) {
+          if (err) {
+            return callback(err, false);
+          }
+
+          console.log(handshakeData.signedCookies);
+          // Get the session for the sessionStore
+          redisStore.get(handshakeData.signedCookies['connect.sid'], function(err, session) {
+            if (err) {
+              return callback(err, false);
+            }
+
+            // Compare the user id in the session with the user id in the request
+            console.log(session.passport.user);
+            if (handshakeData.query.userId !== session.passport.user) {
+              return callback('Unauthorized', false);
+            }
+
+            // Add the user Id to handshake data
+            handshakeData.userId = handshakeData.query.userId;
+            // Process to connect to the socket
+            return callback(null, true);
+          });
+        });
       }
     });
   });
